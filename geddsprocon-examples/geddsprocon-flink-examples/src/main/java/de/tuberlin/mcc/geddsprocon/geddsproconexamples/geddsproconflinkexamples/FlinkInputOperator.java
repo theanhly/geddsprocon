c1b0c929c66e43cb5fff574e7e99ca7ceac58d94
theanhly <theanhly91@gmail.com>
2018-11-09 02:33:10 +0100

    Refactoring and add examples to project

---
+++ geddsprocon-examples/geddsprocon-flink-examples/src/main/java/de/tuberlin/mcc/geddsprocon/geddsproconexamples/geddsproconflinkexamples/FlinkInputOperator.java
@@ -0,0 +1,32 @@
+package de.tuberlin.mcc.geddsprocon.geddsproconexamples.geddsproconflinkexamples;
+
+import de.tuberlin.mcc.geddsprocon.geddsproconcore.DSPConnectorConfig;
+import de.tuberlin.mcc.geddsprocon.geddsproconcore.DSPConnectorFactory;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.api.java.typeutils.TypeInfoParser;
+
+public class FlinkInputOperator {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .addSource((SourceFunction)DSPConnectorFactory.getInstance().createSourceConnector(new DSPConnectorConfig.Builder()
+                        .withDSP("flink")
+                        .withBufferConnectorString("recvbuffer")
+                        .withRequestAddress("192.168.56.102", 9656, DSPConnectorFactory.ConnectorType.SECONDARY)
+                        .withRequestAddress("192.168.56.102", 9666, DSPConnectorFactory.ConnectorType.SECONDARY)
+                        .build()), TypeInfoParser.parse("Tuple2<String,Integer>"))
+                .keyBy("f0")
+                .timeWindow(Time.seconds(5))
+                .sum("f1");
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+    }
+}
